6d6e9773edcce0acfc7cd3a9e8da6aa25b9c35d1
vincetheprogrammer <vincetheprogrammer@gmail.com>
2024-11-11 17:33:38 -0600

    update #notices channel id

--- src/constants/channels.ts
+++ src/constants/channels.ts
@@ -1,7 +1,7 @@
 export enum ChannelId {
     ModTalk = '1294835756925325378',
     Groups = '1297329333395783722',
-    BirthdayAnnounce = '1295537133259329536',
-    WelcomeMessage = '1295537133259329536',
-    Notices = '1295537133259329536'
+    BirthdayAnnounce = '1305675026426761288',
+    WelcomeMessage = '1305675026426761288',
+    Notices = '1305675026426761288'
 }
